587349d8e2fca6ced98184bd9586c5df25eb20b6
Amos Wenger <amos@bearcove.net>
2024-10-09 11:27:53 +0200

    perform upgrade

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use color_eyre::eyre;
-use std::str::FromStr;
+use futures_util::future::BoxFuture;
+use hyper::{
+    body::Body, server::conn, service::Service, upgrade::OnUpgrade, Method, Request, Response,
+    StatusCode,
+};
+use hyper_util::rt::TokioIo;
+use std::{fmt::Debug, str::FromStr};
 use tokio::{
     io::AsyncReadExt,
     net::{TcpListener, TcpStream},
@@ -43,11 +49,65 @@ async fn main() -> eyre::Result<()> {
     Ok(())
 }
 
-async fn handle_stream(mut stream: TcpStream) -> eyre::Result<()> {
-    let mut buf = vec![0; 1024];
-    let n = stream.read(&mut buf).await?;
-    let read_slice = &buf[..n];
-    tracing::debug!("Received:\n{}", pretty_hex::pretty_hex(&read_slice));
+async fn handle_stream(stream: TcpStream) -> eyre::Result<()> {
+    let stream = TokioIo::new(stream);
+    let conn = conn::http1::Builder::new()
+        .serve_connection(stream, ProxyService)
+        .with_upgrades();
+    conn.await?;
+    Ok(())
+}
 
-    eyre::bail!("Not implemented");
+struct ProxyService;
+
+impl<ReqBody> Service<Request<ReqBody>> for ProxyService
+where
+    ReqBody: Body + Debug + Send + 'static,
+{
+    type Response = hyper::Response<String>;
+    type Error = hyper::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn call(&self, req: Request<ReqBody>) -> Self::Future {
+        Box::pin(async move {
+            if req.method() != Method::CONNECT {
+                return Ok(Response::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .body(String::from("we're a forward proxy, we only serve CONNECT"))
+                    .unwrap());
+            }
+
+            tracing::trace!(
+                "Got CONNECT to {}, headers = {:#?}",
+                req.uri(),
+                req.headers()
+            );
+
+            let on_upgrade = hyper::upgrade::on(req);
+            tokio::spawn(async move {
+                if let Err(e) = handle_upgraded_conn(on_upgrade).await {
+                    tracing::error!("Error handling upgraded conn: {e:?}");
+                }
+            });
+
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .body(String::from(
+                    "you're connected, prepare to accept an invalid TLS cert because we're MITM'ing today"
+                ))
+                .unwrap())
+        })
+    }
+}
+
+async fn handle_upgraded_conn(on_upgrade: OnUpgrade) -> eyre::Result<()> {
+    let c = on_upgrade.await.unwrap();
+    let mut c = TokioIo::new(c);
+
+    let mut buf = vec![0u8; 1024];
+    let n = c.read(&mut buf).await?;
+    let read_slice = &buf[..n];
+    tracing::trace!("Read: {}", pretty_hex::pretty_hex(&read_slice));
+
+    Ok(())
 }
